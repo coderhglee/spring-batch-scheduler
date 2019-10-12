1f624c9f58db92b229bce5971097ed04e00986bf
coderhglee <goodna17@gmail.com>
2019-10-12 17:56:43 +0900

    - 스케쥴링 추가 - 비동기 추가

--- src/main/java/com/hglee/batch/job/SimpleJobConfiguration.java
+++ src/main/java/com/hglee/batch/job/SimpleJobConfiguration.java
@@ -1,10 +1,14 @@
 package com.hglee.batch.job;
 
 
+import com.hglee.batch.config.LoggingTaskDecorator;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +16,8 @@ import org.springframework.context.annotation.Configuration;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Slf4j // log 사용을 위한 lombok 어노테이션
 @RequiredArgsConstructor // 생성자 DI를 위한 lombok 어노테이션
@@ -21,7 +27,36 @@ public class SimpleJobConfiguration {
     @Autowired
     private JobBuilderFactory jobBuilderFactory; // 생성자 DI 받음
     @Autowired
-    private StepBuilderFactory stepBuilderFactory; // 생성자 DI 받음
+    private StepBuilderFactory stepBuilderFactory; // 생성자 DI 받음A
+    @Autowired
+    private JobRepository jobRepository;
+
+    public static final int CORE_TASK_POOL_SIZE = 3;
+    public static final int MAX_TASK_POOL_SIZE = 30;
+    public static final int QUEUE_CAPACITY_SIZE = 10;
+    public static final int CHUNK_AND_PAGE_SIZE = 400;
+
+    @Bean(name = "taskExecutor")
+    public TaskExecutor executor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(CORE_TASK_POOL_SIZE);
+        executor.setMaxPoolSize(MAX_TASK_POOL_SIZE);
+        executor.setQueueCapacity(QUEUE_CAPACITY_SIZE);
+        executor.setTaskDecorator(new LoggingTaskDecorator());
+        executor.setThreadNamePrefix("task-pool-");
+        executor.initialize();
+
+        return executor;
+    }
+
+//    @Bean
+//    public JobLauncher createJobLauncher() throws Exception {
+//        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+//        jobLauncher.setJobRepository(jobRepository);
+//        jobLauncher.afterPropertiesSet();
+//        jobLauncher.setTaskExecutor(executor());
+//        return jobLauncher;
+//    }
 
     @Bean
     public Job simpleJob() {
@@ -35,8 +70,10 @@ public class SimpleJobConfiguration {
         return stepBuilderFactory.get("simpleStep1")
                 .tasklet((contribution, chunkContext) -> {
                     log.info(">>>>> This is Step1");
+                    Thread.sleep(2000);
                     return RepeatStatus.FINISHED;
                 })
+//                .taskExecutor(executor())
                 .build();
     }
 }
---
+++ src/main/java/com/hglee/batch/scheduler/AppScheduler.java
@@ -0,0 +1,61 @@
+package com.hglee.batch.scheduler;
+
+import com.hglee.batch.service.AsyncService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppScheduler {
+
+
+    private static final Logger log = LoggerFactory.getLogger(AppScheduler.class);
+
+    @Autowired
+    private Job job;
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private AsyncService asyncService;
+
+    @Scheduled(cron = "0/10 * * * * *")
+    public void work() throws Exception {
+
+
+        Long runTime = System.currentTimeMillis();
+
+        log.info("Job Started at : {}", runTime);
+
+        JobParameters param =
+                new JobParametersBuilder()
+                        .addLong("runTime", runTime).toJobParameters();
+
+        JobExecution execution = null;
+
+        try {
+
+            execution = jobLauncher.run(job, param);
+
+        } catch (JobExecutionAlreadyRunningException e) {
+            e.printStackTrace();
+        } catch (JobRestartException e) {
+            e.printStackTrace();
+        } catch (JobInstanceAlreadyCompleteException e) {
+            e.printStackTrace();
+        } catch (JobParametersInvalidException e) {
+            e.printStackTrace();
+        }
+
+        log.info("Job finished with status : {}", execution.getStatus());
+        asyncService.findUser("hglee",runTime);
+    }
+}
